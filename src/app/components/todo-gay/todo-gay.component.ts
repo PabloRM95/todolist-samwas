f56265fad56096fa4e6eb5fa9b6d28f6d18e9f76
Mario AS <43919016+marioas10@users.noreply.github.com>
2021-11-14 18:02:12 +0000

    Primer commit

--- src/app/components/todo-gay/todo-gay.component.ts
+++ src/app/components/todo-gay/todo-gay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { nanoid } from 'nanoid';
 import { TODO } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
@@ -8,19 +9,27 @@ import { TodoService } from 'src/app/services/todo.service';
   styleUrls: ['./todo-gay.component.css'],
 })
 export class TodoGayComponent implements OnInit {
-  constructor(private TodoService:TodoService) { }
+  constructor(private TodoService: TodoService) {}
 
   textGay: string = '';
   listaGay: TODO[] = [];
 
-
   ngOnInit(): void {
-    this.TodoService.getAll().then((res: TODO[]) => this.listaGay = res)
-    }
+    this.TodoService.getAll().then((res: TODO[]) => (this.listaGay = res));
   }
 
+  /* addNewTask() {
+    let newTask: TODO = {
+      title: this.textGay,
+      id: nanoid(),
+      completed: false,
+      date: Date.now()
+    }
+    this.listaGay.push(newTask);
+    this.textGay = '';
+  }
 
-
-
-
-
+  deleteTask(id: string) {
+    this.listaGay.filter(task => task.id != id);
+  } */
+}
