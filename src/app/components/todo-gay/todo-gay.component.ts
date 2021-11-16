92d8abcdbdd31ebb316910f1ec8d2c46881e3d24
Marcos <marcoslorpen19@gmail.com>
2021-11-16 17:23:14 +0000

    lo ultimo que hivce

--- src/app/components/todo-gay/todo-gay.component.ts
+++ src/app/components/todo-gay/todo-gay.component.ts
@@ -1,23 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { nanoid } from 'nanoid';
 import { TODO } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-gay',
   templateUrl: './todo-gay.component.html',
-  styleUrls: ['./todo-gay.component.css']
+  styleUrls: ['./todo-gay.component.css'],
 })
 export class TodoGayComponent implements OnInit {
-
   textGay: string = '';
   listaGay: TODO[] = [];
 
-  constructor(private TodoService: TodoService) { }
+  constructor(private TodoService: TodoService) {}
 
   ngOnInit(): void {
-    this.TodoService.getAll().then((res: TODO[]) => res = this.listaGay)
+    this.TodoService.getAll().then((res: TODO[]) => {
+      this.listaGay = res;
+      this.listaGay = this.listaGay.slice(1, 5);
+    });
   }
 
-  
+  addNewTask() {
+    let newTask: TODO = {
+      title: this.textGay,
+      id: nanoid(),
+      completed: false,
+      date: Date.now(),
+    };
 
+    this.TodoService.createOne(newTask).then(res => {
+      this.listaGay.push(newTask);
+    }).catch(e => e)
+    
+    this.textGay = '';
+  }
+
+  deleteTask(id: string) {
+    this.TodoService.deleteOne(id).then(res => {
+      this.listaGay = this.listaGay.filter((task) => task.id != id);
+    })
+  }
+
+  completeTask(id: string) { 
+    let task = this.listaGay.find((todo) => todo.id == id) as TODO;
+    task.completed = !task.completed
+    this.TodoService.modifyOne(task)
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 import { TODO } from '../models/todo';
 
 @Injectable({
@@ -8,7 +9,25 @@ export class TodoService {
 
   constructor() { }
 
+  url = 'https://jsonplaceholder.typicode.com/todos';
+
   getAll(): Promise<TODO[]>{
-    return fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json())
+    return axios.get<TODO[]>(this.url)
+    .then(res => res.data)
   }
+
+  createOne(newTask: TODO){
+    return axios.post(this.url, newTask)
+    .then(res => res.data)
+  }
+
+  deleteOne(id: string){
+    return axios.delete(`${this.url}/${id}`)
+    .then(res => res.data)
+  }
+
+  modifyOne(newTask: TODO){
+    return axios.patch(`${this.url}/${newTask.id}`, newTask)
+  }
+
 }
